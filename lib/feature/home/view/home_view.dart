9b70bbacbb2ba60000a267aef5b206a830823bb3
bayramismail <83179561+bayramismail@users.noreply.github.com>
2023-12-25 21:36:51 +0300

    auto router added

--- lib/feature/home/view/home_view.dart
+++ lib/feature/home/view/home_view.dart
@@ -2,7 +2,10 @@ import 'package:architecture_template_v2/feature/home/view/mixin/home_view_mixin
 import 'package:architecture_template_v2/product/init/config/app_environment.dart';
 import 'package:architecture_template_v2/product/init/language/locale_keys.g.dart';
 import 'package:architecture_template_v2/product/init/product_localization.dart';
+import 'package:architecture_template_v2/product/navigation/app_router.dart';
 import 'package:architecture_template_v2/product/utility/constans/enums/locales.dart';
+import 'package:auto_route/annotations.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:gen/gen.dart';
@@ -10,6 +13,7 @@ import 'package:kartal/kartal.dart';
 
 part 'widget/home_app_bar.dart';
 
+@RoutePage()
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
@@ -33,14 +37,17 @@ class _HomeViewState extends State<HomeView> with HomeViewMixin {
           ),
           const Text('Change language'),
           ElevatedButton(
-            onPressed: () {
-              ProductLocalization.updateLanguage(
-                  context: context, value: Locales.en);
+            onPressed: () async {
+              final response = await context.router.push<bool?>(
+                HomeDetailRoute(
+                  id: "100",
+                ),
+              );
             },
             child: Text(
               LocaleKeys.general_button_save,
               style: context.general.textTheme.titleMedium,
-            ).tr(args: ['deneme']),
+            ).tr(args: ['deneme2']),
           ),
         ],
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'package:architecture_template_v2/feature/home/view/home_view.dart';
 import 'package:architecture_template_v2/product/init/application_initialize.dart';
 import 'package:architecture_template_v2/product/init/product_localization.dart';
 import 'package:architecture_template_v2/product/init/theme/index.dart';
+import 'package:architecture_template_v2/product/navigation/app_router.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
@@ -11,15 +11,17 @@ Future<void> main() async {
 }
 
 class _MyApp extends StatelessWidget {
+  static final _appRouter = AppRouter();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        theme: CustomLightTheme().themeData,
-        darkTheme: CustomDarkTheme().themeData,
-        localizationsDelegates: context.localizationDelegates,
-        supportedLocales: context.supportedLocales,
-        locale: context.locale,
-        title: 'Material App',
-        home: HomeView());
+    return MaterialApp.router(
+      routerConfig: _appRouter.config(),
+      theme: CustomLightTheme().themeData,
+      darkTheme: CustomDarkTheme().themeData,
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+      title: 'Material App',
+    );
   }
 }
